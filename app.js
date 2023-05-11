5aa029193b6e02f246c5e9ee5aa0bba8e4a08023
Kevin Adrian <adrian.kevin@yahoo.com>
2023-05-11 19:13:53 +0800

    debug localhost

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: process.env.URI+"/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
